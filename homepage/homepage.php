89be08064d63625f44836e9f6fcdf1896cd31adc
Acokii <zaffaz1106@gmail.com>
2024-10-31 21:51:41 +0700

    ni homepage gabisa kalo ga login

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -1,7 +1,13 @@
 <?php 
 include("connection.php");
-$fetch_product_query = "SELECT * FROM product";
-$fetch_product = $conn->query($fetch_product_query);
+session_start();
+if($_SESSION['is_login']){
+  $fetch_product_query = "SELECT * FROM product";
+  $fetch_product = $conn->query($fetch_product_query);
+}
+else{
+  header("Location: /uts/login-register/login.php");
+}
 ?>
 
 
@@ -23,31 +29,27 @@ $fetch_product = $conn->query($fetch_product_query);
     <input type="text" placeholder="Cari di Ventura" class="search-bar">
     <div class="icons">
       <a href="/uts/keranjang/keranjang.php"><img src="Keranjang.svg" alt="Cart" class="icon"></a>
-      <a href="uts/profile/profile.php" id="profile-text">Profile</a>
+      <a href="/uts/profil/profile.php" id="profile-text">Profile</a>
     </div>
   </div>
 
   <h2 style="text-align: center;">Produk Rekomendasi</h2>
-
   <div class="product-container">
     <?php
-            if($fetch_product->num_rows > 0) {
-              while($row = $fetch_product->fetch_assoc()) {
-                ?>
-                <div class="product-card">
-                    <a href="/uts/product-detail-page/halaman_produk.php?product_id=<?php echo $row['product_id']; ?>">
-                      <object name="product_image"><img src="<?php echo $row['product_image']; ?>"></object>
-                    </a>
-                    <object name="product_name"><p><?php echo $row['product_name']; ?></p></object>
-                    <object name="product_price"><p class="price">Rp<?php echo number_format($row['product_price'], 0, ',', '.'); ?></p></object>
-                    <button type="submit" name="add_to_cart" value="add-to-cart" class="add-icon">+</button>
-                </div>
-                <?php
-                }
-            }
-            ?>
-        </div>
-
-
+              if($fetch_product->num_rows > 0) {
+                while($row = $fetch_product->fetch_assoc()) {
+                  ?>
+                    <div class="product-card">
+                      <a href="/uts/product-detail-page/halaman_produk.php?product_id=<?php echo $row['product_id']; ?>">
+                        <img src="<?php echo $row['product_image']; ?>">
+                        <p><?php echo $row['product_name']; ?></p>
+                        <p class="price">Rp<?php echo number_format($row['product_price'], 0, ',', '.'); ?></p></object>
+                      </a>
+                    </div>
+                  <?php
+                  }
+              }
+              ?>
+      </div>
 </body>
 </html>
