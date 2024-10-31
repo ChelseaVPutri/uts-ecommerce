91327c519c96a6f17f8948edfce9ff1b20851cf1
Acokii <zaffaz1106@gmail.com>
2024-11-01 01:14:34 +0700

    profile udh jadi ya, fitur cuma keranjang sm logout

---
+++ profil/profile.php
@@ -0,0 +1,37 @@
+<?php
+include "service/connection.php";
+session_start();
+if($_SESSION['is_login']){
+  $usernama = $_SESSION['username'];
+  $email = $_SESSION['email'];
+}
+else{
+  header("Location: /uts/login-register/login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Profile Page</title>
+  <link rel="stylesheet" href="profile.css">
+</head>
+<body>
+<header>
+    <a href="/uts/homepage/homepage.php">
+    <button class="back-button">&#8592;</button></a>
+    <h2 style="text-align: center;">profile</h2>
+</header>
+
+  <div class="container">
+    <div class="profile">
+      <div class="profile-name"><?php echo $usernama ?></div>
+      <div class="profile-email"><?= $email?></div>
+      <a href="/uts/keranjang/keranjang.php" class="button1">Keranjang</a>
+      <a href="service/destroy.php" class="button1">Logout</a>
+    </div>
+  </div>
+</body>
+</html>
